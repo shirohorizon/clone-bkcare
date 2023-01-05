bedf91e35266d5d95cca210bb79701108b28220c
longhrzdev <shiroxhorizon@gmail.com>
2023-01-05 20:52:24 +0700

    font-end header

---
+++ src/assets/images/index.js
@@ -0,0 +1,24 @@
+const images = {
+  logo: require('~/assets/images/logo.svg').default,
+  banner: require('~/assets/images/bookingcare-cover-4.jpg'),
+  noImage: require('~/assets/images/No-Image.png'),
+  googlePlay: require('~/assets/images/googleplay.png'),
+  appStore: require('~/assets/images/appstore.png'),
+  nhaKhoa: require('~/assets/images/104635-khamnhakhoa.png'),
+  chuyenKhoa: require('~/assets/images/133537-khamchuyenkhoa.png'),
+  khamTuXa: require('~/assets/images/133657-khamtuxa.png'),
+  xetNghiem: require('~/assets/images/133744-dichvuxetnghiem.png'),
+  tongQuat: require('~/assets/images/133744-khamtongquat.png'),
+  phauThuat: require('~/assets/images/151930-phau-thuat.jpg'),
+  taiNha: require('~/assets/images/133744-khamtainha.png'),
+  sucKhoeTinhThan: require('~/assets/images/133744-suckhoetinhthan.png'),
+  sucKhoeDoanhNghiep: require('~/assets/images/101157-icon-lich-su.jpg'),
+  coXuongKhop: require('~/assets/images/120331-co-xuong-khop.jpg'),
+  thanKinh: require('~/assets/images/121042-than-kinh.jpg'),
+  tieuHoa: require('~/assets/images/120933-tieu-hoa.jpg'),
+  timMach: require('~/assets/images/120741-tim-mach.jpg'),
+  taiMuiHong: require('~/assets/images/121146-tai-mui-hong.jpg'),
+  cotSong: require('~/assets/images/121215-cot-song.jpg'),
+};
+
+export default images;
--- src/layouts/components/Header/Header.js
+++ src/layouts/components/Header/Header.js
@@ -1,12 +1,88 @@
 import React from 'react';
 import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faBars,
+  faCaretDown,
+  faCircleQuestion,
+  faLanguage,
+} from '@fortawesome/free-solid-svg-icons';
 
 import styles from './Header.module.scss';
+import config from '~/config';
+import Image from '~/components/Image';
+import images from '~/assets/images';
 
 const cx = classNames.bind(styles);
 
 const Header = () => {
-  return <header className={cx('wrapper')}>header</header>;
+  return (
+    <header className={cx('wrapper')}>
+      <div className={cx('grid')}>
+        <div className={cx('row')}>
+          <div className={cx('col', 'inner')}>
+            <div className={cx('actions-left')}>
+              <FontAwesomeIcon className={cx('bar-icon')} icon={faBars} />
+              <Link to={config.routes.home} className={cx('logo-link')}>
+                <Image className={cx('logo')} src={images.logo} alt={'logo'} />
+              </Link>
+            </div>
+            <div className={cx('nav')}>
+              {/* header nav */}
+              <ul className={cx('nav-list')}>
+                <li className={cx('nav-item')}>
+                  <h3 className={cx('nav-title')}>Chuyen khoa</h3>
+                  <p className={cx('nav-detail')}>
+                    Tim bac si theo chuyen khoa
+                  </p>
+                </li>
+                <li className={cx('nav-item')}>
+                  <h3 className={cx('nav-title')}>Chuyen khoa</h3>
+                  <p className={cx('nav-detail')}>
+                    Tim bac si theo chuyen khoa
+                  </p>
+                </li>
+                <li className={cx('nav-item')}>
+                  <h3 className={cx('nav-title')}>Chuyen khoa</h3>
+                  <p className={cx('nav-detail')}>
+                    Tim bac si theo chuyen khoa
+                  </p>
+                </li>
+                <li className={cx('nav-item')}>
+                  <h3 className={cx('nav-title')}>Chuyen khoa</h3>
+                  <p className={cx('nav-detail')}>
+                    Tim bac si theo chuyen khoa
+                  </p>
+                </li>
+              </ul>
+            </div>
+            <div className={cx('actions-right')}>
+              {/* actions 'Supports', 'language' */}
+              <div className={cx('support')}>
+                <FontAwesomeIcon
+                  className={cx('support-icon')}
+                  icon={faCircleQuestion}
+                />
+                <span className={cx('action-title')}>Support</span>
+              </div>
+              <div className={cx('language')}>
+                <FontAwesomeIcon
+                  className={cx('language-icon')}
+                  icon={faLanguage}
+                />
+                <span className={cx('action-title')}>vn</span>
+                <FontAwesomeIcon
+                  className={cx('language-down')}
+                  icon={faCaretDown}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
