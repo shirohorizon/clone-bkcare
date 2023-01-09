bbd8bcc09180cdc16b6a2c246cfd01aedc74e094
longhrzdev <shiroxhorizon@gmail.com>
2023-01-09 21:17:59 +0700

    search by topic

---
+++ src/components/CustomScroll/CustomScroll.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
+import styles from './CustomScroll.module.scss';
+
+const cx = classNames.bind(styles);
+
+const CustomScroll = ({
+  width = '100%',
+  height = '100%',
+  overflowX = false,
+  overflowY = true,
+  children,
+  ...passProps
+}) => {
+  const classes = cx('wrapper', {
+    overflowX,
+    overflowY,
+  });
+  return (
+    <div
+      style={{ width: width, height: height }}
+      className={classes}
+      {...passProps}
+    >
+      {children}
+    </div>
+  );
+};
+
+CustomScroll.propTypes = {
+  width: PropTypes.string,
+  height: PropTypes.string,
+  overflowX: PropTypes.bool,
+  overflowY: PropTypes.bool,
+  children: PropTypes.node.isRequired,
+};
+
+export default CustomScroll;
